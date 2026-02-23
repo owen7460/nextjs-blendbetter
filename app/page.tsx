f57b3aa36e1e920df5f03f76f5b7f3c13c6dbcb8
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-02-23 09:03:31 -0700

    change homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,37 @@
-"use client";
+// "use client";
 
-import LiquidEther from "@/components/LiquidEther";
+// import LiquidEther from "@/components/LiquidEther";
 import Link from "next/link";
 export default function Home() {
   return (
     <>
       <div
-        className="min-h-screen bg-black"
+        className="min-h-screen bg-transparent bg-[url('/images/blendbetterBG.png')] bg-cover bg-center"
         style={{ width: "100%", height: 600, position: "relative" }}
       >
-        <p className=" text-white text-sm max-w-2xl text-center absolute z-100 top-12 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p className=" text-gray-800 text-sm max-w-2xl text-center absolute z-100 top-1/6 left-1/2 -translate-x-1/2 -translate-y-1/2">
           Do you know each year, Canadians waste over 500,000 tonnes of fruit,
           which is equivalent to more than 4,000 Olympic-sized swimming pools
         </p>
         <div className="absolute z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-8">
-          <h1 className="textFirst text-white text-8xl font-bold">
+          <h1 className="textFirst text-gray-800 text-8xl font-bold">
             Blendbetter
           </h1>
-          <p className="textSecond text-white text-2xl max-w-4xl text-center">
+          <p className="textSecond text-gray-800 text-2xl max-w-4xl text-center">
             Build your perfect drinks — smoothies, bubble teas, cocktails — and
             see them come to life instantly.
           </p>
-          <p className="textThird text-white text-lg max-w-2xl text-center">
+          <p className="textThird text-gray-800 text-lg max-w-2xl text-center">
             This app is an AI-powered tool that helps you turn the fruits you
             have at home into delicious smoothies, bubble teas, beverages, or
             cocktails. Even those fruit taste sour, but let's save them together
             and stop wasting fruit!
           </p>
           <div className="flex items-center justify-center gap-8">
-            <span className="buttonFirst text-white bg-transparent border border-white px-6 py-2 rounded-full text-lg font-bold">
-              Learn More
+            <span className="buttonFirst text-gray-800 bg-transparent border border-gray-800 px-6 py-2 rounded-full text-lg font-bold">
+              <Link href="https://owen-ca.com" target="_blank">
+                Know More about Me
+              </Link>
             </span>
             <Link
               href="/dashboard"
@@ -40,7 +42,7 @@ export default function Home() {
           </div>
         </div>
 
-        <LiquidEther
+        {/* <LiquidEther
           colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
           mouseForce={20}
           cursorSize={100}
@@ -59,7 +61,7 @@ export default function Home() {
           // color0="#5227FF"
           // color1="#FF9FFC"
           // color2="#B19EEF"
-        />
+        /> */}
       </div>
     </>
   );
