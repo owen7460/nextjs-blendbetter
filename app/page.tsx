84789c59178c4e6ecc143a6342c0b8f92d320412
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-03-12 20:11:28 -0600

    change background on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div
-        className="min-h-screen bg-transparent bg-[url('/images/blendbetterBG.png')] bg-cover bg-center"
-        style={{ width: "100%", height: 600, position: "relative" }}
-      >
+      <div className="min-h-screen width-full height-full relative bg-transparent bg-[url('/images/blendbetterBG.png')] bg-cover bg-left">
         <p className="text-gray-800 text-sm absolute z-100 bottom-0 left-0 translate-x-1/2 -translate-y-1/2">
           Version 1.0.1
         </p>
-        <p className=" text-gray-800 text-sm max-w-2xl text-center absolute z-100 top-1/6 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p className=" text-zinc-500 text-sm max-w-2xl text-center absolute z-100 top-10 left-1/2 -translate-x-1/2 -translate-y-1/2">
           Do you know each year, Canadians waste over 500,000 tonnes of fruit,
           which is equivalent to more than 4,000 Olympic-sized swimming pools
         </p>
         <div className="absolute z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-8">
           <h1 className="textFirst text-gray-800 text-8xl font-bold">
-            Blendbetter
+            BlendBetter
           </h1>
-          <p className="textSecond text-gray-800 text-2xl max-w-4xl text-center">
+          <p className="textSecond text-zinc-600 text-2xl max-w-4xl text-center">
             Build your perfect drinks — smoothies, bubble teas, cocktails — and
             see them come to life instantly.
           </p>
-          <p className="textThird text-gray-800 text-lg max-w-2xl text-center">
+          <p className="textThird text-zinc-600 text-base max-w-2xl text-center">
             This app is an AI-powered tool that helps you turn the fruits you
             have at home into delicious smoothies, bubble teas, beverages, or
             cocktails. Even those fruit taste sour, but let's save them together
             and stop wasting fruit!
           </p>
           <div className="flex items-center justify-center gap-8">
-            <span className="buttonFirst text-gray-800 bg-transparent border border-gray-800 px-6 py-2 rounded-full text-lg font-bold">
+            <span className="buttonFirst text-zinc-600 bg-transparent border border-gray-800 px-6 py-2 rounded-full text-lg font-bold">
               <Link href="https://owen-ca.com" target="_blank">
                 About Me
               </Link>
